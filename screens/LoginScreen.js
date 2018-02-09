c44ada4ab91f2e0883745fee79e468188c474be3
Robinson L. Legaspi <Robinson.Legaspi@maximintegrated.com>
2018-02-09 14:33:04 +0800

    add logo

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import { ScrollView, TextInput, View, Alert, TouchableOpacity, Text} from 'react-native';
+import { Image, ScrollView, TextInput, View, Alert, Text} from 'react-native';
+import { Button } from 'react-native-elements';
 import { LOGIN_URL, HTTP_HEADER, MAIN_COLOR } from '../constants';
 import { connect } from 'react-redux';
 import KeyboardSpacer from 'react-native-keyboard-spacer';
@@ -31,31 +32,40 @@ class LoginScreen extends Component
   }
 
   render() {
-    const { TextStyle, TextInputStyle, MainContainer, button} = styles;
+    const { LogoStyle, TextStyle, TextInputStyle, MainContainer, SubMainContainer, ButtonStyle} = styles;
     return (
       <ScrollView scrollEnabled={false} contentContainerStyle={MainContainer}>
-        <Text style= {TextStyle}>User Login Form</Text>
-        <TextInput
-          placeholder="Enter User username"
-          onChangeText={username => this.props.setUsername(username)}
-          underlineColorAndroid='transparent'
-          style={TextInputStyle}
-        />
-  
-        <TextInput
-          placeholder="Enter User Password"
-          onChangeText={password => this.props.setPassword(password)}
-          underlineColorAndroid='transparent'
-          style={TextInputStyle}
-          secureTextEntry={true}
-        />
-        <TouchableOpacity
-          onPress={() => this._login()} 
-          style={button} 
-        >
-        <Text style={{color:'#fff'}}> Click Here To Login </Text>
-      </TouchableOpacity>
-        <KeyboardSpacer/>
+        <View style={SubMainContainer}>
+          <View style={{
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+            <Image source={require('../assets/icon.png')} style={LogoStyle} />
+          </View>
+          <Text style={TextStyle}>Beach Front Parking</Text>
+          <TextInput
+            placeholder="Enter User username"
+            onChangeText={username => this.props.setUsername(username)}
+            underlineColorAndroid='transparent'
+            style={TextInputStyle}
+          />
+
+          <TextInput
+            placeholder="Enter User Password"
+            onChangeText={password => this.props.setPassword(password)}
+            underlineColorAndroid='transparent'
+            style={TextInputStyle}
+            secureTextEntry={true}
+          />
+          <Button
+            onPress={() => this._login()}
+            title='LOG IN'
+            buttonStyle={ButtonStyle}
+            containerStyle={{ backgroundColor: 'green' }}
+            textStyle={{ color: '#fff' }}
+          />
+          <KeyboardSpacer />
+        </View>
       </ScrollView>           
     );
   }
@@ -64,31 +74,46 @@ class LoginScreen extends Component
 const styles = {
   MainContainer: {
     justifyContent: 'center',
-    flex:1,
-    margin: 10,
+    flex: 1,
   },
 
-  button: {
-    alignItems: 'center',
+  SubMainContainer: {
+    justifyContent: 'center',
+    flex: 1,
     backgroundColor: MAIN_COLOR,
-    padding: 10
+    padding: 10,
+  },
+
+  ButtonStyle: {
+    marginTop: 20,
+    backgroundColor: '#000',
+    borderColor: "transparent",
+    borderWidth: 0,
+    borderRadius: 25,
   },
   
   TextInputStyle: {
     textAlign: 'center',
     marginBottom: 7,
     height: 40,
-    borderWidth: 1,
-    borderColor: MAIN_COLOR,
+    backgroundColor: '#fff',
     borderRadius: 5 , 
   },
   
   TextStyle: {
     fontSize: 20,
-    color: "#000",
+    color: "#fff",
     textAlign: 'center', 
     marginBottom: 15
   },
+
+  LogoStyle: { 
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    borderWidth: 3,
+    borderColor: '#fff' ,
+  }
 };
 
 const mapStateToProps = ({user}) => ({user});
