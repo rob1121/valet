3b535719d80df392a4df2a8fda4e1bc404a8d0ec
Robinson L. Legaspi <robinson.legaspi@yahoo.com>
2018-03-28 21:11:00 +0800

    wip

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -50,7 +50,6 @@ class LoginScreen extends Component
       password,
     }).then(({data}) => {
       this.setState(() => ({ loading: false }));
-      console.log(data);
       if(data.error) {
         this.setState(() => ({ ...this.state, hasStoredData: false}));
         alert(data.msg);
